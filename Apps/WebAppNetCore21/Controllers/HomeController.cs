cff3de17d1199f5ddcaf136c300d84ad221daf87
Elvis Ayiemba <ayiembaelvis@gmail.com>
2019-02-19 07:41:03 +0300

    samples update

--- Apps/WebAppNetCore21/Controllers/HomeController.cs
+++ Apps/WebAppNetCore21/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using WebApplication1.Models;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
 
 namespace WebApplication1.Controllers
 {
@@ -14,44 +16,70 @@ namespace WebApplication1.Controllers
         
         private readonly IConfiguration _configuration;
         private readonly IMpesaClient _mpesaClient;
+        private readonly IMemoryCache _memoryCache;
+        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(IMpesaClient mpesaClient, IConfiguration configuration)
+        public HomeController(IMpesaClient mpesaClient, IConfiguration configuration, IMemoryCache memoryCache, ILogger<HomeController> logger)
         {
            
             _configuration = configuration;
             _mpesaClient = mpesaClient;
+            _memoryCache = memoryCache;
+            _logger = logger;
         }
+
+        private string ConsumerKey { get { return _configuration["MpesaConfiguration:ConsumerKey"]; } }
+        private string ConsumerSecret { get { return _configuration["MpesaConfiguration:ConsumerSecret"]; } }
+        private string PassKey { get { return _configuration["MpesaConfiguration:PassKey"]; } }
+        private string AccessToken { get { return GetToken(); } }
+
+        //TODO: make this method async
+        private string GetToken()
+        {
+            var cacheKey = "MpesaAccessToken";
+
+            if (_memoryCache.TryGetValue(cacheKey, out string accessToken))
+            {
+                _logger.LogInformation("Getting token from memory....."); 
+                return accessToken;
+            }
+            else
+            {
+                _logger.LogInformation("Getting token from Mpesa Server....."); 
+                accessToken =  _mpesaClient.GetAuthToken(ConsumerKey, ConsumerSecret, RequestEndPoint.AuthToken);
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(59));
+
+                _memoryCache.Set(cacheKey, accessToken, cacheEntryOptions);
+
+                return accessToken;
+            }
+        }
+
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
-        {
-            var consumerKey = _configuration["MpesaConfiguration:ConsumerKey"];
-            
-            var consumerSecret = _configuration["MpesaConfiguration:ConsumerSecret"];
-
-            var passKey = _configuration["MpesaConfiguration:PassKey"];
-
-            //Get Token
-            var accesstoken = _mpesaClient.GetAuthToken(consumerKey, consumerSecret, RequestEndPoint.AuthToken);
-
-            //LipaNaMpesaOnline
+        //Lipa Na Mpesa Online - STK Push
+        public async Task<IActionResult> LNMO()
+        {                   
             LipaNaMpesaOnlineDto lipaOnline = new LipaNaMpesaOnlineDto
             {
-                AccountReference = "test",
-                Amount = "1",
-                PartyA = "254725589166",
+                AccountReference = "Sample",
+                Amount = "2",                   //replace the amount you want to test. Max is KSh 70,000 per transaction
+                PartyA = "",        //replace this number with yours or clients safcom number
                 PartyB = _configuration["MpesaConfiguration:LNMOshortCode"],
                 BusinessShortCode = _configuration["MpesaConfiguration:LNMOshortCode"],
                 CallBackURL = _configuration["MpesaConfiguration:CallBackUrl"],
-                PhoneNumber = "254725589166",
-                Passkey = passKey,
-                TransactionDesc = "test"
+                PhoneNumber = "",   //replace this number with yours or clients safcom number
+                Passkey = PassKey,
+                TransactionDesc = "MpesaLib Sample"
 
             };
-            var lipaNaMpesa = _mpesaClient.MakeLipaNaMpesaOnlinePayment(lipaOnline, accesstoken, RequestEndPoint.LipaNaMpesaOnline);
+
+            var lipaNaMpesa = await _mpesaClient.MakeLipaNaMpesaOnlinePaymentAsync(lipaOnline, AccessToken, RequestEndPoint.LipaNaMpesaOnline);
 
 
             ViewData["Message2"] = lipaNaMpesa;
